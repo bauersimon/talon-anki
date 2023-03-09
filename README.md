9bfe643d8ff82e6290320beb39b43b7ce2e0a4b9
Simon Bauer <sbauer@cs.uni-salzburg.at>
2023-03-09 11:53:15 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # talon-anki
-Memorize talon commands with Anki.
+
+Memorize [talon](https://talonvoice.com/) commands with [Anki](https://apps.ankiweb.net/).
+
+## Currently Supported
+
+- talon alphabet
+
+## Downloading
+
+- `.csv` directly from GitHub
+- `.apkg` via [bauersimon.github.io/talon-anki](https://bauersimon.github.io/talon-anki)
+
+## Adding Commands
+
+Add a `.csv` under `decks/csv` and run `builder.py`.
---
+++ builder.py
@@ -0,0 +1,54 @@
+import csv
+import random
+from os import listdir, path, makedirs
+from typing import List, Tuple
+from sys import argv
+
+import genanki
+
+
+def read_csv(p: str) -> List[Tuple[str, str]]:
+    with open(p) as f:
+        reader = csv.reader(f, delimiter=',')
+        return list(reader)
+
+
+CARD = genanki.Model(
+    123456789,
+    'QA Model',
+    fields=[
+        {'name': 'Question'},
+        {'name': 'Answer'},
+    ],
+    templates=[
+        {
+            'name': 'Card',
+            'qfmt': '{{Question}}',
+            'afmt': '{{FrontSide}}<hr>{{Answer}}',
+        },
+    ])
+
+
+def save_deck(items: List[Tuple[str, str]], p: str):
+    deck = genanki.Deck(
+        random.randrange(1 << 30, 1 << 31),
+        path.splitext(path.basename(p))[0])
+    for i in items:
+        deck.add_note(genanki.Note(
+            model=CARD,
+            fields=[i[0], i[1]]))
+    genanki.Package(deck).write_to_file(p)
+
+
+def batch(source: str, target: str):
+    for f in listdir(source):
+        d = read_csv(path.join(source, f))
+        save_deck(d, path.join(target, path.splitext(f)[0]+'.apkg'))
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        makedirs(path.join('decks', 'pkg'), exist_ok=True)
+        batch(path.join('decks', 'pkg'), path.join('decks', 'apkg'))
+    else:
+        batch(argv[1], argv[2])
